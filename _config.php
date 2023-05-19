66e99be2b7d579047df41a634d4768ef519d62a2
AdhmAlateiat <47896904+AdhmAlateiat@users.noreply.github.com>
2023-05-19 10:22:25 -0400

    Update _config.php

--- _config.php
+++ _config.php
@@ -2,7 +2,7 @@
 $conn = mysqli_connect("localhost", 'root' , '', "anikatsu") or die("Connection fail");
 
 
-$websiteTitle = "AniKatsu"; // Website Name
+$websiteTitle = "LRMAnime"; // Website Name
 $websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
 $websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
 $contactEmail = "xyz@gmail.com"; // Contact Email
